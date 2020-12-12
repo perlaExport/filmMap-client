8e16e68e18ce38504377a1c037ceb61a90e1a4e5
Darek <35233347+DaRoTP@users.noreply.github.com>
2020-12-12 18:35:04 +0100

    Forgot password (#16)
    closes #16

---
+++ src/components/forms/Register/RegistrationSuccess.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./RegistrationSuccess.scss";
+import SuccessCheck from "components/general/SuccessCheck/SuccessCheck";
+
+interface RegistrationSuccessProps {
+    email: string,
+    changeFormSceneToLogin: () => void
+}
+
+const RegistrationSuccess: React.FC<RegistrationSuccessProps> = ({ email, changeFormSceneToLogin }) => {
+    return (
+        <div className="registration-success">
+            <SuccessCheck />
+            <span className="successfull">Your registration was successful!</span>
+            <span className="instructions">
+                {`Activate your account using the link sent to `}
+            </span>
+            <strong className="email">"DRodzewicz@gmail.com"</strong>
+            <button type="button" className="link-element login-nav-link" onClick={changeFormSceneToLogin}>
+                go to Login
+            </button>
+        </div>
+    )
+}
+
+export default RegistrationSuccess
