7701bd5d2e43e66cc7fb69093da0fd1491fe5890
DaRoBB <drodzewicz@gmail.com>
2020-12-20 12:32:33 +0100

    created a wrapper helper functoion for callAPI fetching helper function to do TMDB fetching

---
+++ src/helper/apiCall.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+interface callAPIParams {
+    url: string, 
+    method: "GET" | "POST" | "PUT" | "DELETE", 
+    token?: "string", 
+	payload?: any, 
+	headers?: any,
+	queryParams?: object,
+    setLoading?: (loadingState: boolean) => void
+}
+
+const convertObjToQueryString = (objectQuery: object) => {
+	if (objectQuery === {} ) return "";
+	let queryString = "";
+	for (const [key, value] of Object.entries(objectQuery)) {
+		queryString += queryString === "" ? "?" : "&"
+		queryString += `${key}=${value}`;
+	}
+	return queryString;
+}
+
+const callAPI = async ({ url, method, token, payload, setLoading, headers: passedHeaders, queryParams}: callAPIParams) => {
+	let headers = {};
+	let requestURL = url;
+
+	if(!!passedHeaders) headers = passedHeaders;
+	if (!!token) headers = { ...headers, Authorization: localStorage.getItem("token") };
+
+	if(!!queryParams && queryParams !== {}) requestURL += convertObjToQueryString(queryParams);
+
+	!!setLoading && setLoading(true);
+	try {
+		const res = await axios({ method, url: requestURL, data: payload, headers });
+        !!setLoading && setLoading(false);
+		return { data: res.data, error: null, status: res.status };
+	} catch (error) {
+		!!setLoading && setLoading(false);
+		return { data: null, error: error.response?.data, status: error.response?.status };
+	}
+};
+
+export default callAPI;
