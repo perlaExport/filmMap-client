c0b5e7ecf8bcaa7484aaefbf14954ef26e75b5f2
DaRoBB <drodzewicz@gmail.com>
2020-12-27 17:32:43 +0100

    genreList compoenent that displays movie genres

--- src/pages/MovieDetails/MovieDetails.tsx
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -1,19 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import "./MovieDetails.scss";
 import callTMDBAPI from "helper/apiCallTMDB";
-// import Image from "components/general/Image/Image";
+import Image from "components/general/Image/Image";
 import PosterBackdrop from "./PosterBackdrop/PosterBackdrop";
 import LoadingWrapper from "components/layout/LoadingWrapper/LoadingWrapper";
+import GenreList from "./GenreList/GenreList";
+
 
 interface MovieDetailProps {
     movieId: number
 }
 
+interface Genres {
+    id: number,
+    name: string
+}
+
+interface MovieDetails {
+    id: number,
+    posterPath?: string,
+    backdropPath?: string,
+    genres: Genres[],
+    overview: string,
+    title: string
+}
+
 const MovieDetails: React.FC<MovieDetailProps> = ({ movieId }) => {
 
-    const [movieDetails, setMoviedetails] = useState<string>("")
-    const [isLoading, setLoading] = useState<boolean>(true)
-
+    const [movieDetails, setMoviedetails] = useState<MovieDetails>({
+        id: 0,
+        posterPath: "",
+        backdropPath: "",
+        genres: [],
+        overview: "",
+        title: ""
+    })
 
     useEffect(() => {
 
@@ -21,11 +42,11 @@ const MovieDetails: React.FC<MovieDetailProps> = ({ movieId }) => {
             const { data, status, error } = await callTMDBAPI({
                 url: `/movie/${movieId}`,
                 method: "GET",
-                setLoading
             }); 
             console.log(data, status, error);
             if(status === 200) {
-                setMoviedetails(data.backdrop_path);
+                const {id, poster_path, backdrop_path, genres, overview, title } = data;
+                setMoviedetails({ id, posterPath: poster_path, backdropPath: backdrop_path, genres, overview,title });
             }
         }
         if(!!movieId) getMovieDetailsBydId();
@@ -35,14 +56,21 @@ const MovieDetails: React.FC<MovieDetailProps> = ({ movieId }) => {
     }, [movieId])
 
     return (
-        <div className="movie-details-page">
-            <LoadingWrapper isLoading={isLoading}>
-                {movieDetails !== "" && <PosterBackdrop posterPath={`http://image.tmdb.org/t/p/w1280${movieDetails}`} />}
+            <LoadingWrapper className="movie-details-page" isLoading={movieDetails.title === ""}>
+                {movieDetails.backdropPath !== "" && <PosterBackdrop
+                                                        posterImageLink={`http://image.tmdb.org/t/p/w1280${movieDetails.posterPath}`}
+                                                        backdropImageLink={`http://image.tmdb.org/t/p/w1280${movieDetails.backdropPath}`} />}
                 <section className="main-movie-details">
-                    test
+                    <div className="poster-wrapper">
+                        <Image imageURL={`http://image.tmdb.org/t/p/w185${movieDetails.posterPath}`} className="movie-poster" />
+                    </div>
+                    <div className="movie-info">
+                        <h1 className="movie-title">{movieDetails.title}</h1>
+                        <GenreList genres={movieDetails.genres} />
+                        <p className="movie-overview">{movieDetails.overview}</p>
+                    </div>
                 </section>
             </LoadingWrapper>
-        </div>
     )
 }
 
