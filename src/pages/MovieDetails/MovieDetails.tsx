4afe47fd847b85ff60fe313120d1fb50f97101fc
DaRoBB <drodzewicz@gmail.com>
2020-12-27 13:15:50 +0100

    chnaged from TMDB v4 to v3 & movieDetails page gets movieId in params

--- src/pages/MovieDetails/MovieDetails.tsx
+++ src/pages/MovieDetails/MovieDetails.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./MovieDetails.scss";
+import callTMDBAPI from "helper/apiCallTMDB";
 
-const MovieDetails: React.FC = () => {
+interface MovieDetailProps {
+    movieId: number
+}
+
+const MovieDetails: React.FC<MovieDetailProps> = ({ movieId }) => {
+
+    useEffect(() => {
+
+        const getMovieDetailsBydId = async () => {
+            const { data, status, error } = await callTMDBAPI({
+                url: `/movie/${movieId}`,
+                method: "GET",
+            }); 
+            console.log(data, status, error);
+        }
+        if(!!movieId) getMovieDetailsBydId();
+        
+        return () => { 
+        }
+    }, [movieId])
+
     return (
         <div className="movie-details-page">
             Movie Details
