50a8629208bad4b9359fdfd01212b525c2b274ff
DaRoBB <drodzewicz@gmail.com>
2021-01-03 17:32:30 +0100

    my ratings subpage

--- src/pages/Profile/ProfileSubPages/MyRatings.tsx
+++ src/pages/Profile/ProfileSubPages/MyRatings.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react';
+import MovieCard from "components/general/MovieCard/MovieCard";
+import Pagination from "components/general/Pagination/Pagination";
+import LoadingWrapper from "components/layout/LoadingWrapper/LoadingWrapper";
+
+
+const testMovies = [
+    {id: 1, title: "title 1", imageURL: "test"},
+    {id: 2, title: "title 2", imageURL: "test"},
+    {id: 3, title: "title 3", imageURL: "test"},
+    {id: 4, title: "title 4", imageURL: "test"},
+    {id: 5, title: "title 5", imageURL: "test"},
+    {id: 6, title: "title 6", imageURL: "test"},
+    {id: 7, title: "title 7", imageURL: "test"},
+    {id: 8, title: "title 8", imageURL: "test"},
+    {id: 9, title: "title 9", imageURL: "test"},
+    {id: 10, title: "title 10", imageURL: "test"},
+]
+
+interface PageProps {
+    currentPage: number,
+    amountOfPages: number
+}
 
 const MyRatings: React.FC = () => {
+    const [page, setPage] = useState<PageProps>({ currentPage: 1, amountOfPages: 1});
+
+    const handleChangePage = (newPage: number) => {
+        setPage({...page, currentPage: newPage})
+    }
+
     return (
-        <div className="my-ratings-subpage-container">
-            MyRatings
+        <div  className="my-ratings-subpage-container movie-list-container">
+            <LoadingWrapper isLoading={false} className="movies">
+                {testMovies.map(({ id, title, imageURL}) => (
+                    <MovieCard key={id} movieId={id} posterImageURL={imageURL} title={title}  />
+                ))}
+            </LoadingWrapper>
+            <Pagination currentPage={page.currentPage} handleChange={handleChangePage} amountOfPages={page.amountOfPages} />
         </div>
     )
 }
