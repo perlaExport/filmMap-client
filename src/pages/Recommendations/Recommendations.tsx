afddb9a2ba624e32d8bf9cff3fef8959c70954b7
DaRoBB <drodzewicz@gmail.com>
2021-01-14 13:02:13 +0100

    top 5 movie recommendation page

--- src/pages/Recommendations/Recommendations.tsx
+++ src/pages/Recommendations/Recommendations.tsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Recommendations.scss";
+import MovieCard, { MovieCardProps } from "components/general/MovieCard";
+import LoadingWrapper from "components/layout/LoadingWrapper";
+import callAPI from "helper/api";
 
 const Recommendations: React.FC = () => {
-  return <div className="recommendations-page">Recommendations</div>;
+  const { REACT_APP_TMDB_IMAGE_BASE_URL } = process.env;
+
+  const [movies, setMovies] = useState<MovieCardProps[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const getMyRatedMovies = async () => {
+      const { data, status } = await callAPI({
+        url: "/movie/favourites?limit=5&page=0",
+        method: "GET",
+        token: true,
+        setLoading,
+      });
+      if (status === 200) {
+        setMovies(
+          data.movies.map(
+            ({ id, title, imgPath }: { id: number; title: string; imgPath?: string }) => ({
+              movieId: id,
+              title,
+              posterImageURL: imgPath,
+            })
+          )
+        );
+      }
+    };
+    getMyRatedMovies();
+    return () => {};
+  }, []);
+  return (
+    <LoadingWrapper isLoading={isLoading} className="recommended-movies">
+      <h2 className="page-title">
+        <span className="top">Top 5 movies</span>recommended for you
+      </h2>
+      {movies.map(({ movieId, title, posterImageURL }) => (
+        <MovieCard
+          key={movieId}
+          movieId={movieId}
+          title={title}
+          posterImageURL={
+            !!posterImageURL ? `${REACT_APP_TMDB_IMAGE_BASE_URL}/w185${posterImageURL}` : ""
+          }
+        />
+      ))}
+    </LoadingWrapper>
+  );
 };
 
 export default Recommendations;
