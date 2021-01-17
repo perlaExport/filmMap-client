55fb359e2ed722603c5fcd4e5820fe1f0d5a5120
DaRoBB <drodzewicz@gmail.com>
2021-01-17 12:39:22 +0100

    if user provides bad id in /movie/{id} it will redirect teh user to /error page

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Error.scss";
+
+const Error: React.FC = () => {
+  return (
+    <div className="error-page">
+      <h2 className="error-message">Not Found</h2>
+    </div>
+  );
+};
+
+export default Error;
